bd97631ff5d7fc5c1f9a8788f8bfee9c9f175c37
suny-webdevs <mdsunyshaikh@gmail.com>
2025-03-08 01:01:48 +0600

    add ribbon badge in product card

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Badge, Button } from "antd";
 import { Link } from "react-router-dom";
 import { TBicycle } from "../../interface/global";
 
@@ -7,31 +7,33 @@ type TProductCardProps = {
 };
 
 const ProductCard = ({ item }: TProductCardProps) => {
-  const { image, name, price, _id, brand, category } = item;
+  const { image, name, price, _id, brand, category, inStock } = item;
 
   return (
-    <div className="group border-x border-gray-200 p-5">
-      <Link to={`${import.meta.env.VITE_BASE_URL}/bicycles/${_id}`}>
-        <div className="flex h-48 items-center">
-          <img src={image} alt={name} />
-        </div>
-        <div>
-          <h1 className="text-lg group-hover:text-[#FA5252]">{name}</h1>
-          <p className="text-sm tracking-wider text-gray-500">
-            {brand} &middot; {category}
-          </p>
-          <h1
-            style={{ fontWeight: 500 }}
-            className="mt-1 text-lg font-bold text-[#FA5252]"
-          >
-            ${price}
-          </h1>
-        </div>
-      </Link>
-      <Button style={{ marginTop: "1.5rem" }} type="primary" block>
-        ADD TO CART
-      </Button>
-    </div>
+    <Badge.Ribbon text={inStock ? "In-Stock" : "Sold-Out"} color="#FA5252">
+      <div className="group border-x border-gray-200 p-5">
+        <Link to={`${import.meta.env.VITE_BASE_URL}/bicycles/${_id}`}>
+          <div className="flex h-48 items-center">
+            <img src={image} alt={name} />
+          </div>
+          <div>
+            <h1 className="text-lg group-hover:text-[#FA5252]">{name}</h1>
+            <p className="text-sm tracking-wider text-gray-500">
+              {brand} &middot; {category}
+            </p>
+            <h1
+              style={{ fontWeight: 500 }}
+              className="mt-1 text-lg font-bold text-[#FA5252]"
+            >
+              ${price}
+            </h1>
+          </div>
+        </Link>
+        <Button style={{ marginTop: "1.5rem" }} type="primary" block>
+          ADD TO CART
+        </Button>
+      </div>
+    </Badge.Ribbon>
   );
 };
 
